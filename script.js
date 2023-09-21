85052ba62c6aa5e89663f0d6281572c83d6b644a
Ivan G <igutierrez0794@gmail.com>
2023-09-21 15:20:11 -0400

    added styling to middle content, and functionality to the temperatures changing

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ function weatherDisplay(){
     alert("Loading weather report...")
 }
 
-function onTempChange(){
-    console.log("you changed temp")
+function celcius2Fahrenheit(temp){
+    return Math.round(9 / 5 * temp + 32);
 }
+
+function fahrenheit2Celcius(temp){
+    return Math.round(5 / 9 * (temp - 32));
+}
+
+function onTempChange(element){
+    console.log("you changed temp")
+    console.log(element.value)
+    for(var i=1; i<9; i++){
+        var spanTag = document.querySelector("#temp-" + i);
+        var tempValue = parseInt(spanTag.innerText);
+        if(element.value == "C"){
+            spanTag.innerText = fahrenheit2Celcius(tempValue);
+        } else {
+            spanTag.innerText = celcius2Fahrenheit(tempValue);
+        }
+    }
+}
+
